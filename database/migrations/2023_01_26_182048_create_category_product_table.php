7eac4cc27368dcfc0e9bc6d4a483253605d4a632
Mikey Ying <mikeyinguk@gmail.com>
2023-01-26 18:32:02 +0000

    fixed bug in pivot table migration

--- database/migrations/2023_01_26_182048_create_category_product_table.php
+++ database/migrations/2023_01_26_182048_create_category_product_table.php
@@ -15,9 +15,7 @@ return new class extends Migration
     {
         Schema::create('category_product', function (Blueprint $table) {
             $table->id();
-            $table->integer('category_id');
             $table->foreignId('category_id')->constrained()->cascadeOnDelete();
-            $table->integer('product_id');
             $table->foreignId('product_id')->constrained()->cascadeOnDelete();
         });
     }
