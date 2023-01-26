9c25a8af41a7540150997d222717a543cef84873
Mikey Ying <mikeyinguk@gmail.com>
2023-01-26 18:49:29 +0000

    re-added timestamps to migrations

--- database/migrations/2023_01_26_181341_create_products_table.php
+++ database/migrations/2023_01_26_181341_create_products_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->decimal('price');
             $table->integer('quantity');
             $table->string('description')->nullable();
+            $table->timestamps();
         });
     }
 
