54a7ee4f35c3309669fda145acad1608bef5d428
Mikey Ying <mikeyinguk@gmail.com>
2023-01-26 18:16:21 +0000

    updated product migration to include required fields

--- database/migrations/2023_01_26_181341_create_products_table.php
+++ database/migrations/2023_01_26_181341_create_products_table.php
@@ -15,7 +15,10 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('name');
+            $table->decimal('price');
+            $table->integer('quantity');
+            $table->string('description')->nullable();
         });
     }
 
