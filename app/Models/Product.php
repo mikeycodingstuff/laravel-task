ed6399f5f8e96dec89b9aea7b9d869c2b6b7de68
Mikey Ying <mikeyinguk@gmail.com>
2023-01-26 18:41:01 +0000

    added many to many relationship to Category and Product models

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,4 +10,9 @@ class Product extends Model
     use HasFactory;
 
     protected $fillable = ['name', 'price', 'quantity', 'description'];
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
 }
